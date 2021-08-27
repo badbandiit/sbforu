d952942a1cf70f0f556a720eb7ba8bca751eda60
zigzzag <59981732+zigz0g@users.noreply.github.com>
2021-08-27 12:12:54 -0400

    Create hide.js

---
+++ hide.js
@@ -0,0 +1,4 @@
+$(window).keydown(function(h) { 
+    d[e.which] = true; 
+    $('#body').css('background-image','url(https://www.howtogeek.com/wp-content/uploads/2018/07/400-error-header_2-1.png?width=1198&trim=1,1&bg-color=000&pad=1,1)');
+});
